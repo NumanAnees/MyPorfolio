d077938a25a2fdb4a596dcb9fcc5912c1ee05f87
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-05-20 23:29:37 +0500

    Projects updated

--- form/contact.php
+++ form/contact.php
@@ -6,8 +6,8 @@ $message= $_POST['message'];
 $to = "numananees44@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: rfeder76@gmail.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$headers = "From: bsef19a506@pucit.edu.pk" . "\r\n" .
+"CC: bsef19a506@pucit.edu.pk";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
